371e828e1cfacdccfb9f20c8d27db4d0d5e9f7d6
akashmishra92735 <akashmishra92735@gmail.com>
2025-05-21 09:02:02 +0530

    Add files via upload

---
+++ tuesday.js
@@ -0,0 +1,40 @@
+a="hello";
+console.log(a);
+let a;
+
+let x="40";
+let y=10;
+console.log(x+y);
+
+let a=30;
+let b=10;
+console.log(a-b);
+
+let m=23;
+let m1=2;
+console.log(m*m1);
+
+let d=24;
+let d1=4;
+console.log(d/d1);
+
+let mod=64;
+let modu=8;
+console.log(mod%modu);
+
+
+let k=10.5;
+let k1=3.7;
+console.log(k%k1);  
+
+let x="40";
+let y=10;
+console.log(x===y);
+
+let a=30;
+let b=10;
+console.log(a>=b);
+
+let r=23;
+let w=45;
+console.log(a !==b);
