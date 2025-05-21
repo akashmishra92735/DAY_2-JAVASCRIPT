0a7dd78e41f9991cbe3eccfe415119f3a04b3d08
akashmishra92735 <akashmishra92735@gmail.com>
2025-05-21 09:03:51 +0530

    Add files via upload

--- tuesday.js
+++ tuesday.js
@@ -1,40 +1,52 @@
+                     //HOSTING IN JS
+     //Hosting is a default behaviour of javascript where all the variable & function-
+     //-declarations are moved on top.
 a="hello";
 console.log(a);
 let a;
-
-let x="40";
+       //OPERATORS
+//(i)Arithmetic Operators(+,-,*,/,%)
+let x=40;
 let y=10;
-console.log(x+y);
+console.log(x+y);//adds x and y (40+10) and prints 50 to the console.
 
 let a=30;
 let b=10;
-console.log(a-b);
+console.log(a-b);//subtracts a and b (30-10) and prints 20 to the console.
 
 let m=23;
 let m1=2;
-console.log(m*m1);
+console.log(m*m1);//multiply m and m1 (23*2)and prints 46 to the console.
 
 let d=24;
 let d1=4;
-console.log(d/d1);
+console.log(d/d1);//divided d and d1 (24/4) and prints 6 to the console.
 
-let mod=64;
-let modu=8;
-console.log(mod%modu);
+let mod = 10;
+let modu = 3;
+console.log(mod % modu);// it returns remainder.
 
-
-let k=10.5;
-let k1=3.7;
-console.log(k%k1);  
-
+//(ii)Comparison Operators(==,===,!=,!==,>,<,>=,<=)
 let x="40";
 let y=10;
-console.log(x===y);
+console.log(x==y);//Check the value that equal or not.
+
+let p="40";
+let q=10;
+console.log(p===q);//Two equal symbol check the value and third is checking tha data-
+    //type.If bothe condition is true then output is true otherwise output is false.
 
 let a=30;
 let b=10;
-console.log(a>=b);
+console.log(a!=b);//if the value is same then output is false.
 
 let r=23;
 let w=45;
-console.log(a !==b);
+console.log(a !==b);//If any one condition is true then output true.
+
+let d=30;
+let k=10;
+console.log(d<k);//If condition is true then output is true otherwise false.
+console.log(k>b);//If condition is true then output is true otherwise false.
+console.log(k<=b);//If any one condition is true then output true.
+console.log(k>=b);//If any one condition is true then output true.
